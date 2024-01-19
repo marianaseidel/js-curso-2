387a5802ae6d04c81baae664e4ac81deb86631f6
Mariana Seidel <marianaaseidel@gmail.com>
2024-01-19 11:22:21 -0300

    challenge resolution 1

---
+++ app.js
@@ -0,0 +1,24 @@
+let titulo = document.querySelector('h1');
+
+titulo.innerHTML = 'Hora do Desafio'
+
+function exibirConsole() {
+    console.log('O botão foi clicado');
+}
+
+function exibirAlerta() {
+    alert('Eu amo JS');
+}
+
+function exibirPrompt(){
+    let cidade = prompt('Digite o nome de uma cidade do Brasil');
+    alert(`Estive em ${cidade} e lembrei de você`);
+}
+
+function soma() {
+    let valor1 = prompt('Digite um número');
+    let valor2 = prompt('Digite outro número');
+    let resultado = valor1 + valor2;
+
+    alert(`O resultado da soma de ${valor1} e ${valor2} é ${resultado}`);
+}
