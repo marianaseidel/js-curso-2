364d0d35e2097f9f1002d7b8cca33874dc5ec2d8
Mariana Seidel <marianaaseidel@gmail.com>
2024-01-21 10:53:49 -0300

    update project

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function exibirPrompt(){
 function soma() {
     let valor1 = prompt('Digite um número');
     let valor2 = prompt('Digite outro número');
-    let resultado = valor1 + valor2;
+    let resultado = parseInt(valor1) + parseInt(valor2);
 
-    alert(`O resultado da soma de ${valor1} e ${valor2} é ${resultado}`);
+    alert(`O resultado da soma de ${valor1} e ${valor2} é igual a ${resultado}`);
 }
